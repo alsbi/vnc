3e60a06400e9ff8d03db8b7d605db694b8bfba89
alsbi <alsbi@iMac.local>
2015-12-06 23:13:41 +0300

    fix /console/ url action

--- vnc_viewer/engine/service/__init__.py
+++ vnc_viewer/engine/service/__init__.py
@@ -3,7 +3,7 @@ __author__ = 'alsbi'
 
 import os
 
-from flask import Flask, render_template, send_from_directory, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for
 
 from ..virshlike import Manager
 from vnc_viewer.engine.errors import *
@@ -19,23 +19,10 @@ mn = Manager()
 @app.route('/consoles')
 def get_domain_list():
     if 'username' in session:
-        return render_template('consoles.html', domain_list = mn.get_active_domain(), info = mn.status(),
+        return render_template('consoles.html',
+                               domain_list = mn.get_active_domain(),
+                               info = mn.status(),
                                domain_list_inactive = mn.get_inactive_domain())
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/console/<vm_name>')
-def get_domain_by_uid(vm_name):
-    if 'username' in session:
-        try:
-            return render_template('console.html', domain_list = mn.get_active_domain(), id = vm_name,
-                                   domain_list_inactive = mn.get_inactive_domain(), host = HOST_REMOTE_VIRSH,
-                                   port = mn.get_vnc_port_by_uuid(vm_name),
-                                   password = mn.set_vnc_pass_by_uuid(vm_name))
-        except Error_update_domain as e:
-            if not mn.get_domain_by_uuid(vm_name).isActive():
-                return "<div style='background-color:white'>Connection lost</div>"
-            return repr(e)
     else:
         return redirect(url_for('login'))
 
@@ -43,27 +30,33 @@ def get_domain_by_uid(vm_name):
 @app.route('/console/<vm_name>/<action>')
 def action_domain_by_uid(vm_name, action):
     if 'username' in session:
-        if action in ['stop', 'start', 'reboot', 'shutdown', 'reset']:
+        if action in ['stop', 'start', 'restart', 'shutdown', 'reset']:
             getattr(mn, '%s_domain' % action)(vm_name)
-        try:
-            return redirect(url_for('get_domain_by_uid', vm_name = vm_name, host = HOST_REMOTE_VIRSH,
-                                port = mn.get_vnc_port_by_uuid(vm_name),
-                                password = mn.set_vnc_pass_by_uuid(vm_name)))
-        except Error_update_domain as e:
-                if not mn.get_domain_by_uuid(vm_name).isActive():
-                    return "<div style='background-color:white'>Connection lost</div>"
-                return repr(e)
+        return view_domain(vm_name)
     else:
         return redirect(url_for('login'))
 
 
-# debug
-@app.route('/view/<vm_name>/<path:path>')
-def get_static(vm_name, path):
+@app.route('/console/<vm_name>')
+def domain_by_uid(vm_name):
     if 'username' in session:
-        return send_from_directory('static', path)
+        return view_domain(vm_name)
     else:
         return redirect(url_for('login'))
+
+
+def view_domain(vm_name):
+    password = ''
+    try:
+        password = mn.set_vnc_pass_by_uuid(vm_name)
+    except Error_update_domain as e:
+        password = ''
+    return render_template('console.html',
+                           host = HOST_REMOTE_VIRSH,
+                           domain_list = mn.get_active_domain(), id = vm_name,
+                           domain_list_inactive = mn.get_inactive_domain(),
+                           port = mn.get_vnc_port_by_uuid(vm_name),
+                           password = password)
 
 
 @app.route('/login', methods = ['GET', 'POST'])
